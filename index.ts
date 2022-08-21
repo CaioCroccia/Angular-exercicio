14636290674b1a0ea9bde9415794f202a0f3ee2f
CaioCroccia <caio.carvalho15@fatec.sp.gov.br>
2022-08-21 20:56:05 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -9,8 +9,13 @@ appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 //Metodo tabata
 // 20s de exercicios e 10s de descando = 1 ciclo -> total 5 ciclos
 
-
-for(let count = 0; count <= 20; count++){
+let count = 0;
+for(let count = 1; count <= 20; count++){
   console.log(count);
 }
-
+if(count = 20){
+  console.log('descanso');
+  for(let count = 10; count <= 11; count--){
+    console.log(count);
+  }
+}
