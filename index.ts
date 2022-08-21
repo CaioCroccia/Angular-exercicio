a9dc53fc6257e88fc545df0783ffe55b1d4703a6
CaioCroccia <caio.carvalho15@fatec.sp.gov.br>
2022-08-21 20:52:39 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -4,3 +4,13 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+
+//Metodo tabata
+// 20s de exercicios e 10s de descando = 1 ciclo -> total 5 ciclos
+
+
+for(let count = 0; count <= 20; count++){
+  console.log(count);
+}
+
